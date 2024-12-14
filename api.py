f624b118da9f49a7c0ae1925ad3f82eae9785037
alexander <cyberpunk77.ti@gmail.com>
2024-12-13 22:50:30 -0300

    api.py

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
+"""
+Script para probar que la libreria funcione bien con la api del mikrotik que se este usando
+"""
+
 import routeros_api
 
 # Configuración de conexión
-mikrotik_host = "192.168.88.1"
+mikrotik_host = "192.168.88.1" # ip del mikrotik
 mikrotik_username = "apipython"  # Cambia por tu usuario
 mikrotik_password = "Admin12345."  # Cambia por tu contraseña
 
@@ -11,12 +15,12 @@ try:
         mikrotik_host,
         username=mikrotik_username,
         password=mikrotik_password,
-        port=8728,
+        port=8728, # puerto de servicio de la api del mikrotik
         plaintext_login=True
     )
     api = connection.get_api()
 
-    # Obtener el address list
+    # Obtener el address list (  para verificar que funciona bien con la api y no hay fallas )
     address_list = api.get_resource('/ip/firewall/address-list')
     items = address_list.get()
     for item in items:
